3e0c6f24c04f112c1b81cc4378bf5e89c16d8e46
Ulfa32 <ulfarahmawati@students.amikom.ac.id>
2023-04-05 15:58:10 +0700

    Add project files.

---
+++ Tugas PlaneTicket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tugas_PlaneTicket;
+
+namespace tugas_planetiket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlaneTicket ticket1 = new PlaneTicket();
+            PlaneTicket ticket2 = new PlaneTicket();
+
+            ticket1.Origin = "Jogja";
+            ticket1.Destination = "YIA";
+            ticket1.Cost = 650000;
+            ticket1.Currency = "IDR";
+
+            ticket2.Origin = "Solo";
+            ticket2.Destination = "PKU";
+            ticket2.Cost = 250;
+            ticket2.Currency = "USD";
+
+            ticket1.PrintSummary();
+            Console.WriteLine();
+            ticket2.PrintSummary();
+            Console.ReadKey();
+        }
+    }
+}
